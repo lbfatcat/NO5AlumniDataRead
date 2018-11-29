70bd7f4299aa1a9322fd6b2f5e9b1fd703df572f
liangbin <liangbin@sharinglearn.com>
2018-11-29 12:58:28 +0800

    first build the project in eclipse;

---
+++ src/NO5AlumniDataRead.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+/**
+ * @author liangbin
+ * 从五中的校友录文件中按行读取数据，调用函数判别当前的内容，并写入目标数据文件当中；
+ */
+public class NO5AlumniDataRead {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Data processed.");
+		return;
+	}
+
+}
