9e8bfec95548f67d2dcb447ad8f949c08a0a8855
梁彬 <liangbin@sharinglearn.com>
2018-11-29 13:03:00 +0800

    Frist build the project. However, i don't know how to operate directly in eclipse without help from CLI.

--- src/NO5AlumniDataRead.java
+++ src/NO5AlumniDataRead.java
@@ -12,7 +12,7 @@ public class NO5AlumniDataRead {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Data processed.");
+		System.out.println("Data processed. Goodbye!");
 		return;
 	}
 
